e98e94bd8b048a6578f06c670347a47672db864a
vchaillo <valentinchaillou89@gmail.com>
2016-08-12 00:49:48 +0200

    update readme

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 * [Subject] (https://github.com/vchaillo/snowcrash/blob/master/snow_crash.pdf)
 
+### Todo
+- level08 a revoir
+- level09 a revoir
+
 =============
 Project of 42
--- level09/Ressources/level09.c
+++ level09/Ressources/level09.c
@@ -9,12 +9,12 @@ int main(int argc, char **argv)
 	char	buff[50];
 	int		res;
 
-	i = 0;
 	if (argv[1])
 	{
 		fd = open(argv[1], O_RDONLY);
 		res = read(fd, &buff, 50);
 		buff[res] = 0;
+		i = 0;
 		while (buff[i])
 		{
 			printf("%c", buff[i] - i);
